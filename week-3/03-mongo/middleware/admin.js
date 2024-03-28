6b5b4183b3f1a43b0937b13392bfa15b2007a5c7
Masudur Rahman <rmasudur815@gmail.com>
2024-03-28 12:22:22 -0700

    middles for user and admin authentication done of 03-mongo assingment

--- week-3/03-mongo/middleware/admin.js
+++ week-3/03-mongo/middleware/admin.js
@@ -1,7 +1,29 @@
+const { Admin } = require("../db");
+
 // Middleware for handling auth
 function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+    let username = req.body.username;
+    let password = req.boyd.password;
+
+    //check if the user name exist ont he admin db
+    Admin.findOne({
+        username: username,
+        password: password
+    })
+        //after checking for user pass
+        .then(value => {
+            //if value is true allows access via next()
+            if (value) {
+                next()
+            } else {
+                //eles they dont have access to the admin route
+                res.status(403).json({
+                    msg: "admin dosent exist"
+                })
+            }
+        })
 }
 
 module.exports = adminMiddleware;
